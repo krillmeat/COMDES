7930a1611f2a47934b2c74c76f40137d2a6a2eed
Ross <conover.ross@gmail.com>
2021-03-30 13:02:59 -0500

    boilerplate

---
+++ JS/modals.js
@@ -0,0 +1,25 @@
+class MODALS{
+  constructor(container){
+    this._container = container;
+
+    this.addLaunchEvents(document.querySelectorAll("button.meet-me"))
+  }
+
+  addLaunchEvents(nodeList){
+    let obj = this;
+    for(let i = 0; i < nodeList.length; i++){
+      nodeList[i].addEventListener("click",this.launchModal.bind(obj));
+    }
+  }
+
+  launchModal(e){
+    let t = e.currentTarget;
+    let match = document.querySelector(`.modal[data-label='${t.dataset.label}']`);
+
+    document.querySelector(".modal[data-state='active']").dataset.state = 'inactive';
+    match.dataset.state = 'active';
+
+  }
+
+  get container(){ return this._container }
+}
--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,10 @@
+var modals;
 window.onload = function(){
     init();
 }
 
 function init(){
     console.log("%cInitializing...","color:#999");
+
+    modals = new MODALS(document.querySelector(".modal-container"));
 }
