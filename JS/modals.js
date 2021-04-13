22db122e4ca29dbada5c9f374cd14eab71296415
Ross <conover.ross@gmail.com>
2021-04-13 08:40:00 -0500

    buncha changes

--- JS/modals.js
+++ JS/modals.js
@@ -1,6 +1,8 @@
 class MODALS{
   constructor(container){
     this._container = container;
+    this.currentModal = null;
+    this.launchScroll = 0;
 
     this.addLaunchEvents(document.querySelectorAll("button.meet-me"))
     this.addCloseEvents(document.querySelectorAll("button.close-modal"))
@@ -25,13 +27,27 @@ class MODALS{
   launchModal(e){
     let t = e.currentTarget;
     let match = document.querySelector(`.modal[data-label='${t.dataset.label}']`);
+    this.currentModal = match;
+
+    // Visibility Stuff
+    if(window.scrollY > 900){
+      // show side guy
+    }
 
     if(document.querySelectorAll(".modal[data-state='active']").length > 0) document.querySelector(".modal[data-state='active']").dataset.state = 'inactive';
-    match.dataset.state = 'active';
+    match.style.display = window.innerWidth >= 700 ? "flex" : "block";
+    setTimeout(function(){match.dataset.state = 'active';},100);
+
+    // Height stuff
+    let topCalc = ( (window.innerHeight - match.offsetHeight) / 2 ) - 32;
+    match.style.marginTop = topCalc <= 0 ? "8px" : topCalc + "px";
 
     match.querySelector("button.close-modal").focus();
 
+    this.launchScroll = window.scrollY;
+    document.body.style.top = (0 - window.scrollY )+"px";
     document.body.dataset.state = 'modal';
+
     document.querySelector(".modal-container").dataset.state = 'show';
   }
 
@@ -48,13 +64,34 @@ class MODALS{
     if(kc === 27 && document.body.dataset.state === 'modal'){
       document.querySelector(".modal[data-state='active']").dataset.state = 'inactive';
       this.closeModal();
+    } else if(kc === 9 && document.body.dataset.state === 'modal'){
+      if(!e.shiftKey){
+        if(document.activeElement.classList.contains("portfolio-link")){
+          e.preventDefault();
+          this.currentModal.querySelector('.close-modal').focus();
+        }
+      } else{
+        if(document.activeElement.classList.contains('close-modal')){
+          e.preventDefault();
+          console.log("? ",document.activeElement);
+          this.currentModal.querySelector('.portfolio-link').focus();
+          console.log("? ",document.activeElement);
+        }
+      }
     }
   }
 
   closeModal(){
-    document.body.dataset.state = 'default';
+    let obj = this;
+    document.documentElement.style.scrollBehavior = "auto";
     setTimeout(function(){
       document.querySelector(".modal-container").dataset.state = '';
+      obj.currentModal.style.display = "none";
+      obj.currentModal = null;
+      document.body.dataset.state = 'default';
+      document.body.style.top = "0px";
+      window.scrollTo(0,obj.launchScroll);
+      document.documentElement.style.scrollBehavior = "smooth";
     },400);
   }
 
